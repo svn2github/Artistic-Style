91d92e821d180f500fca00d38d2185f65b78592f
jimp03 <jimp03@1fe3c263-5997-42ff-936f-87a7378ef0cd>
2009-03-14 17:27:37 +0000

    Initial import.
    git-svn-id: https://svn.code.sf.net/p/astyle/code@22 1fe3c263-5997-42ff-936f-87a7378ef0cd

---
+++ trunk/AStyleDev/src-s/AStyleInterface.cs
@@ -0,0 +1,95 @@
+// AStyleInterface.cs
+
+using System;
+using System.Runtime.InteropServices;
+
+/// AStyleInterface contains methods to call the Artistic Style formatter.
+public class AStyleInterface
+{
+    // Cannot use String as a return value because Mono runtime will attempt to
+    // free the returned pointer resulting in a runtime crash.
+    [DllImport("astyle", CallingConvention = CallingConvention.StdCall)]
+    private static extern IntPtr AStyleMain
+    (
+        [MarshalAs(UnmanagedType.LPStr)] String sIn,
+        [MarshalAs(UnmanagedType.LPStr)] String sOptions,
+        AStyleErrorDelgate errorFunc,
+        AStyleMemAllocDelgate memAllocFunc
+    );
+
+    // Cannot use String as a return value because Mono runtime will attempt to
+    // free the returned pointer resulting in a runtime crash.
+    [DllImport("astyle", CallingConvention = CallingConvention.StdCall)]
+    private static extern IntPtr AStyleGetVersion();
+
+    // AStyleMain callbacks
+    private delegate IntPtr AStyleMemAllocDelgate(int size);
+    private delegate void AStyleErrorDelgate
+    (
+        int errorNum,
+        [MarshalAs(UnmanagedType.LPStr)] String error
+    );
+
+    // AStyleMain Delegates
+    private AStyleMemAllocDelgate AStyleMemAlloc;
+    private AStyleErrorDelgate AStyleError;
+
+    /// Declare callback functions
+    public AStyleInterface()
+    {
+        AStyleMemAlloc = new AStyleMemAllocDelgate(OnAStyleMemAlloc);
+        AStyleError = new AStyleErrorDelgate(OnAStyleError);
+    }
+
+    /// Call the AStyleMain function in Artistic Style.
+    /// An empty string is returned on error.
+    public String FormatSource(String textIn, String options)
+    {
+        // Return the allocated string
+        // Memory space is allocated by OnAStyleMemAlloc, a callback function from AStyle
+        String sTextOut = String.Empty;
+        try {
+            IntPtr pText = AStyleMain(textIn, options, AStyleError, AStyleMemAlloc);
+            if (pText != IntPtr.Zero) {
+                sTextOut = Marshal.PtrToStringAnsi(pText);
+                Marshal.FreeHGlobal(pText);
+            }
+        } catch (DllNotFoundException e) {
+            Console.WriteLine(e.ToString());
+        } catch (EntryPointNotFoundException e) {
+            Console.WriteLine(e.ToString());
+        }
+        return sTextOut;
+    }
+
+    /// Get the Artistic Style version number.
+    /// An empty string is returned on error.
+    public String GetVersion()
+    {
+        String sVersion = String.Empty;
+        try {
+            IntPtr  pVersion = AStyleGetVersion();
+            if (pVersion != IntPtr.Zero) {
+                sVersion = Marshal.PtrToStringAnsi(pVersion);
+            }
+        } catch (DllNotFoundException e) {
+            Console.WriteLine(e.ToString());
+        } catch (EntryPointNotFoundException e) {
+            Console.WriteLine(e.ToString());
+        }
+        return sVersion;
+    }
+
+    // Allocate the memory for the Artistic Style return string.
+    private IntPtr OnAStyleMemAlloc(int size)
+    {
+        return Marshal.AllocHGlobal(size);
+    }
+
+    // Display errors from Artistic Style .
+    private void OnAStyleError(int errorNumber, String errorMessage)
+    {
+        Console.WriteLine("astyle error " + errorNumber + " - " + errorMessage);
+    }
+
+}   // class AStyleInterface
---
+++ trunk/AStyleDev/src-s/Example.cs
@@ -0,0 +1,125 @@
+// Example.cs
+
+using System;
+using System.IO;
+using System.Text;
+
+/// Example opens the source files, calls the AStyleInterface methods
+/// to format the files, and saves the reformatted source. The files
+/// are in a test-s directory. The option mode=cs must be included
+/// for C# files.
+public class Example
+{
+    /// Main function for Example
+    public static void Main(string[] args)
+    {
+        // files to pass to AStyle
+        String[] fileName =  { "../test-s/FileUtility.cs",
+                               "../test-s/MainClass.cs" ,
+                               "../test-s/StringParser.cs" ,
+                             };
+
+        // options to pass to AStyle
+        // mode=cs is required for C# files
+        String options = "brackets=linux mode=cs";
+
+        // create an object
+        AStyleInterface AStyle = new AStyleInterface();
+
+        // get Artistic Style version
+        // does not need to terminate on an error
+        String version = AStyle.GetVersion();
+        if (version != String.Empty)
+            Console.WriteLine ("AStyle Version " + version);
+
+        // process the files
+        for (int i = 0; i < fileName.Length; i++) {
+            // get the text to format
+            String textIn = GetText(fileName[i]);
+
+            // call the Artistic Style formatting function
+            // does not need to terminate on an error
+            String textOut = AStyle.FormatSource(textIn, options);
+            if (textOut == String.Empty) {
+                Console.WriteLine("cannot format "  + fileName[i]);
+                continue;
+            }
+
+            // return the formatted text
+            Console.WriteLine("formatted " + fileName[i]);
+            SetText(textOut, fileName[i]);
+        }
+
+        return;
+    }
+
+    ///  Error message function for this example.
+    private static void Error(String why, String what)
+    {
+        Console.WriteLine(why + ' ' + what);
+        Console.WriteLine("The program has terminated!");
+        Environment.Exit(1);
+    }
+
+    ///  Get the text to be formatted.
+    ///  Usually the text would be obtained from an edit control.
+    private static String GetText(string fileName)
+    {
+        // create input buffers
+        int readSize = 1024;
+        StringBuilder bufferIn = new StringBuilder(readSize);
+        char[] fileIn = new char[readSize];
+
+        // read file data
+        try
+        {
+            FileStream file = new FileStream(fileName, FileMode.Open);
+            StreamReader streamIn = new StreamReader(file);
+            // use ReadBlock to preserve the current line endings
+            int charsIn = streamIn.ReadBlock(fileIn, 0, readSize);
+            while (charsIn != 0)
+            {
+                bufferIn.Append(fileIn, 0, charsIn);
+                charsIn = streamIn.ReadBlock(fileIn, 0, readSize);
+            }
+            streamIn.Close();
+        } catch (DirectoryNotFoundException e) {
+            Console.WriteLine(e.ToString());
+            Error("Cannot find directory", fileName);
+        } catch (FileNotFoundException e) {
+            Console.WriteLine(e.ToString());
+            Error("Cannot find file", fileName);
+        } catch (Exception e) {
+            Console.WriteLine(e.ToString());
+            Error("Error reading file", fileName);
+        }
+
+        return bufferIn.ToString();
+    }
+
+    ///  Return the formatted text.
+    ///  Usually the text would be returned to  an edit control.
+    private static void SetText(String textOut, String fileName)
+    {
+        // create a backup file
+        String origFileName = fileName +  ".orig";
+        File.Delete(origFileName);                  // remove a pre-existing file
+        FileInfo outFile = new FileInfo(fileName);
+        outFile.MoveTo(origFileName);
+
+        // write the output file - same name as input
+        try {
+            char[] bufferOut = textOut.ToCharArray();
+            FileStream file = new FileStream(fileName, FileMode.Create);
+            StreamWriter streamOut = new StreamWriter(file);
+            streamOut.Write(bufferOut, 0, bufferOut.Length);
+            streamOut.Close();
+        } catch (Exception e) {
+            Console.WriteLine(e.ToString());
+            Error("Error writing file", fileName);
+        }
+
+        return;
+    }
+
+}   // class Example
