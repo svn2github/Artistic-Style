af81083b7f50ee7c88a8d595ac7a0921e6ac3967
jimp03 <jimp03@1fe3c263-5997-42ff-936f-87a7378ef0cd>
2009-03-14 17:26:51 +0000

    Initial import.
    git-svn-id: https://svn.code.sf.net/p/astyle/code@15 1fe3c263-5997-42ff-936f-87a7378ef0cd

---
+++ trunk/AStyleDev/file-sh/astyle-gcc.sh
@@ -0,0 +1,3 @@
+bindir=../build/cb-gcc/bin
+
+$bindir/astyle  -taOP  "../test-c/*.cpp"  "../test-c/*.h"
---
+++ trunk/AStyleDev/file-sh/astyle-intel.sh
@@ -0,0 +1,3 @@
+bindir=../build/cb-intel/bin
+
+$bindir/astyle  -taOP  "../test-c/*.cpp"  "../test-c/*.h"
