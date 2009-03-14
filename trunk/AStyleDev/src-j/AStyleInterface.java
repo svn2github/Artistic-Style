dc1712ad1e71f04dcd30c7d3beb0352e9103e741
jimp03 <jimp03@1fe3c263-5997-42ff-936f-87a7378ef0cd>
2009-03-14 17:27:24 +0000

    Initial import.
    git-svn-id: https://svn.code.sf.net/p/astyle/code@20 1fe3c263-5997-42ff-936f-87a7378ef0cd

---
+++ trunk/AStyleDev/src-j/AStyleInterface.java
@@ -0,0 +1,103 @@
+// AStyleInterface.java
+
+/**
+* AStyleInterface contains methods to call the Artistic Style formatter.
+* Changing the class name requires changing Artistic Style.
+*/
+
+class AStyleInterface
+{
+    /**
+    *  Call the AStyleMain function in Artistic Style.
+    *  @param   textIn   A string containing the source code to be formatted.
+    *  @param   options  A string of options to Artistic Style.
+    *  @return  A String containing the formatted source from Artistic Style,
+    *           or an empty string on error.
+    */
+    public String formatSource(String textIn, String options)
+    {
+        // Return the allocated string
+        // Memory space is allocated by OnAStyleMemAlloc, a callback function from AStyle
+        String textOut = new String("");
+        try {
+            textOut = AStyleMain(textIn, options);
+        } catch (UnsatisfiedLinkError e) {
+//            System.out.println(e.getMessage());
+            System.out.println("cannot call function AStyleMain");
+        }
+        return textOut;
+    }
+
+    /**
+    *  Call the AStyleGetVersion function in Artistic Style.
+    *  @return  A String containing the formatted source from Artistic Style,
+    *           or an empty string on error.
+    */
+    public String getVersion()
+    {
+        String version = new String();
+        try {
+            version = AStyleGetVersion();
+        } catch (UnsatisfiedLinkError e) {
+//            System.out.println(e.getMessage());
+            System.out.println("cannot call function GetVersion");
+        }
+        return version;
+    }
+
+    // functions to call Artistic Style ---------------------------------------------------
+
+    /**
+    * Static constructor to load the native Artistic Style library.
+    * Does not need to terminate if the shared library fails to load.
+    * But the exception must be handled when a function is called.
+    */
+    static
+    {
+        // load shared library from the classpath
+        String astylePath = System.getProperty("java.class.path")
+                            + System.getProperty("file.separator")
+                            + System.mapLibraryName("astylej");
+        try {
+            System.load(astylePath);
+        } catch (UnsatisfiedLinkError e) {
+//            System.out.println(e.getMessage());
+            System.out.println("cannot load library " + astylePath);
+        }
+    }
+
+    /**
+    *  Calls the AStyleMain function in Artistic Style.
+    *
+    *  @param   textIn   A string containing the source code to be formatted.
+    *  @param   options  A string of options to Artistic Style.
+    *  @return  A String containing the formatted source from Artistic Style.
+    */
+    public native String AStyleMain(String textIn, String options);
+
+    /**
+    * Calls the AStyleGetVersion function in Artistic Style.
+    *
+    * @return    A String containing the version number of Artistic Style.
+    */
+    public native String AStyleGetVersion();
+
+    /**
+    * Error handler for messages from Artistic Style.
+    * This method is called only if there are errors when AStyleMain is called.
+    * This is for debugging and there should be no errors when the calling
+    *  parameters are correct.
+    * Changing the method name requires changing Artistic Style.
+    * Signature: (ILjava/lang/String;)V
+    *
+    *  @param  errorNumber   The error number from Artistic Style.
+    *  @param  errorMessage  The error message from Artistic Style.
+    */
+    private void ErrorHandler(int errorNumber, String errorMessage)
+    {
+        System.out.println("astyle error "
+                           + String.valueOf(errorNumber)
+                           + " - " + errorMessage);
+    }
+
+}   // class AStyleInterface
---
+++ trunk/AStyleDev/src-j/Example.java
@@ -0,0 +1,125 @@
+// Example.java
+
+/*
+* Example opens the source files, calls the AStyleInterface methods
+* to format the files, and saves the reformatted source. The files
+* are in a test-j directory. The option mode=java must be included
+* for java files.
+*/
+
+import java.io.*;
+
+public class Example
+{
+    public static void main(String[] args)
+    {
+        // files to pass to AStyle
+        String fileName[] =  { "../test-j/JSBeautifier.java",
+                               "../test-j/JSFormatter.java" ,
+                               "../test-j/JSLineBreaker.java" ,
+                             };
+
+        // options to pass to AStyle
+        // mode=java is required for Java files
+        String options = "style=java mode=java";
+
+        // create an object
+        AStyleInterface astyle = new AStyleInterface();
+
+        // get Artistic Style version
+        // does not need to terminate on an error
+        String version = astyle.getVersion();
+        if (version.length() != 0)
+            System.out.println("AStyle Version " + version);
+
+        // process the files
+        for (int i = 0; i < fileName.length; i++) {
+            // get the text to format
+            String textIn = getText(fileName[i]);
+
+            // call the Artistic Style formatting function
+            // does not need to terminate on an error
+            String textOut = astyle.formatSource(textIn, options);
+            if (textOut.length() == 0) {
+                System.out.println("cannot format "  + fileName[i]);
+                continue;
+            }
+
+            // return the formatted text
+            System.out.println("formatted " + fileName[i]);
+            setText(textOut, fileName[i]);
+        }
+        return;
+    }
+
+    /*
+    *  Error message function for this example.
+    */
+    private static void error(String why, String what)
+    {
+        System.out.println(why + ' ' + what);
+        System.out.println("The program has terminated!");
+        System.exit(1);
+    }
+
+    /*
+    *  Get the text to be formatted.
+    *  Usually the text would be obtained from an edit control.
+    */
+    private static String getText(String fileName)
+    {
+        // create input buffers
+        File inFile = new File(fileName);
+        int readSize = 1024;
+        StringBuffer bufferIn = new StringBuffer(readSize);
+        char fileIn[] = new char[readSize];
+
+        // read file data
+        try {
+            BufferedReader in =
+                new BufferedReader(new FileReader(inFile));
+            // use read to preserve the current line endings
+            int charsIn = in.read(fileIn, 0, readSize);
+            while (charsIn != -1) {
+                bufferIn.append(fileIn, 0, charsIn);
+                charsIn = in.read(fileIn, 0, readSize);
+            }
+            in.close();
+        } catch (Exception e) {
+            if (e instanceof FileNotFoundException)
+                error("Could not open input file", fileName);
+            else if (e instanceof IOException)
+                error("Error reading file", fileName);
+            else
+                error(e.getMessage(), fileName);
+        }
+
+        return bufferIn.toString();
+    }
+
+    /*
+    *  Return the formatted text.
+    *  Usually the text would be returned to  an edit control.
+    */
+    private static void setText(String textOut, String fileName)
+    {
+        // create a backup file
+        String origFileName = fileName +  ".orig";
+        File origFile = new File(origFileName);
+        File outFile = new File(fileName);
+        origFile.delete();                  // remove a pre-existing file
+        if (!outFile.renameTo(origFile))
+            error("Could not create backup file", origFileName);
+
+        // write the output file - same name as input
+        try {
+            BufferedWriter out =
+                new BufferedWriter(new FileWriter(fileName));
+            out.write(textOut, 0, textOut.length());
+            out.close();
+        } catch (IOException e) {
+            error("Could not write to output", fileName);
+        }
+    }
+
+}   // class Example
