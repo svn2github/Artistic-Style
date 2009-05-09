186eedf70d28e1a7187561b3ed76135026bc6024
jimp03 <jimp03@1fe3c263-5997-42ff-936f-87a7378ef0cd>
2009-05-09 18:54:40 +0000

    Update scripts.
    git-svn-id: https://svn.code.sf.net/p/astyle/code@98 1fe3c263-5997-42ff-936f-87a7378ef0cd

--- trunk/AStyleDev/file-bat/profile-print.bat
+++ trunk/AStyleDev/file-bat/profile-print.bat
@@ -1,12 +1,21 @@
 @echo off
+REM Run gprof to print the profile report
+REM AStylep must be run to create the gmon.out file
 
 set progdir=..\build\cb-mingw\bin
 set mingwdir=C:\Programs\MinGW\bin
 
+REM check for the compiled program
+if not exist gmon.out  (
+echo Must run AStylep to create gmon.out!
+pause
+exit 100
+)
 
 REM do NOT use recursive
 %mingwdir%\gprof  "%progdir%\AStylep.exe"  > profile.txt
 
-
+echo -
+echo end of gprof
 echo -
 pause
--- trunk/AStyleDev/file-bat/profile-run.bat
+++ trunk/AStyleDev/file-bat/profile-run.bat
@@ -1,13 +1,23 @@
 @echo off
+REM Run AStylep executable to create the gmon.out file
+REM Code must be compiled with the Profile target in MinGW AStyle
 
 set progdir=..\build\cb-mingw\bin
 set testdir=..\..\TestData\CodeBlocks\src
+
+REM check for the compiled program
+if not exist "%progdir%\AStylep.exe"  (
+echo Must compile AStyle with the Profile configuration!
+pause
+exit 100
+)
 
 
 REM do NOT use recursive
 :: "%progdir%\AStylep"   -CSKBNLwM50m10FyexpPUoOcEv  "%testdir%\ASBeautifier.cpp"
 :: "%progdir%\AStylep"   -CSKBNLwM50m10FyexpPUoOcEv  "%testdir%\ASBeautifier.java"
 "%progdir%\AStylep"   -CSKBNLwM50m10FyexpPUoOcERv  "%testdir%\*.cpp"
+
 
 echo -
 pause
