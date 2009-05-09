973429e21d3b67d5c8656598254dae817e3c6f5b
jimp03 <jimp03@1fe3c263-5997-42ff-936f-87a7378ef0cd>
2009-05-09 18:28:02 +0000

    Update scripts.
    git-svn-id: https://svn.code.sf.net/p/astyle/code@92 1fe3c263-5997-42ff-936f-87a7378ef0cd

--- trunk/AStyleTest/file-bat/cb-test.bat
+++ trunk/AStyleTest/file-bat/cb-test.bat
@@ -9,10 +9,17 @@ set testdata=..\..\TestData
 :: set astyle=..\..\AStyle\build\vs2005\bin\AStyle
 set astyle=..\..\AStyle\build\vs2008\bin\AStyled
 
+<<<<<<< .mine
+set options= -HvRQ
+:: set options= -bCSKBGNLwM50m10yeoOcFpPHUxEvRQ
+REM without indent-blocks or indent-brackets (B or G)
+:: set options= -gCSKNLwM50m10yeoOcFpPHUxEvRQ
+=======
 set options= -HvRQ
 :: set options= -CSKBGNLwM50m10yeoOcFpPHUxEvRQ
 REM without indent-blocks or indent-brackets (B or G)
 :: set options= -CSKNLwM50m10yeoOcFpPHUxEvRQ
+>>>>>>> .r91
 
 
 call  libCompile.bat  %astyle%
