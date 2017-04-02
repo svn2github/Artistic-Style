96fb9b864d1e4d40d83703a2f70974ef8441bbdc
jimp03 <jimp03@1fe3c263-5997-42ff-936f-87a7378ef0cd>
2017-04-02 18:52:44 +0000

    Final changes for version 3.0.
    git-svn-id: https://svn.code.sf.net/p/astyle/code@570 1fe3c263-5997-42ff-936f-87a7378ef0cd

--- trunk/AStyleDev/file-bat/dist-copy.bat
+++ trunk/AStyleDev/file-bat/dist-copy.bat
@@ -1,7 +1,7 @@
 @REM make example program distribution directories
 @echo off
 
-set distdir=..\DistExamples
+set distdir=..\..\DistExamples
 set distcpp=example-cpp
 set distjava=example-java
 set distobjc=example-objc
@@ -16,7 +16,7 @@ if not exist %distdir%  md  %distdir%
 cd %distdir%
 
 REM remove orig files in test-data
-if exist ..\test-data\*.orig  del ..\test-data\*.orig  /q
+if exist ..\AStyleDev\test-data\*.orig  del ..\AStyleDev\test-data\*.orig  /q
 
 REM copy astyle-cpp
 echo.
@@ -25,15 +25,15 @@ if not exist %distcpp%  md  %distcpp%
 if exist %distcpp%\doc\*  del %distcpp%\doc\*  /q
 if exist %distcpp%\src-c\*  del %distcpp%\src-c\*  /q
 if exist %distcpp%\test-data\*  del %distcpp%\test-data\*  /q
-xcopy ..\doc\cpp.html  %distcpp%\doc\  /q
+xcopy ..\AStyleDev\doc\cpp.html  %distcpp%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\aiseered.css  %distcpp%\doc\  /q
+xcopy ..\AStyleDev\doc\aiseered.css  %distcpp%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\styles.css  %distcpp%\doc\  /q
+xcopy ..\AStyleDev\doc\styles.css  %distcpp%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\src-c\*.cpp  %distcpp%\src-c\  /q
+xcopy ..\AStyleDev\src-c\*.cpp  %distcpp%\src-c\  /q
 if errorlevel 2 pause
-xcopy ..\test-data\*    %distcpp%\test-data\  /q
+xcopy ..\AStyleDev\test-data\*    %distcpp%\test-data\  /q
 if errorlevel 2 pause
 "%zippath%"  "a"  "%distcpp%.zip"  "%distcpp%\*" > NUL
 echo %distcpp%.zip created
@@ -46,15 +46,15 @@ if not exist %distjava%  md  %distjava%
 if exist %distjava%\doc\*  del %distjava%\doc\*  /q
 if exist %distjava%\src-j\*  del %distjava%\src-j\*  /q
 if exist %distjava%\test-data\*  del %distjava%\test-data\*  /q
-xcopy ..\doc\jni.html  %distjava%\doc\  /q
+xcopy ..\AStyleDev\doc\jni.html  %distjava%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\greenlcd.css  %distjava%\doc\  /q
+xcopy ..\AStyleDev\doc\greenlcd.css  %distjava%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\styles.css  %distjava%\doc\  /q
+xcopy ..\AStyleDev\doc\styles.css  %distjava%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\src-j\*.java  %distjava%\src-j\  /q
+xcopy ..\AStyleDev\src-j\*.java  %distjava%\src-j\  /q
 if errorlevel 2 pause
-xcopy ..\test-data\*  %distjava%\test-data\  /q
+xcopy ..\AStyleDev\test-data\*  %distjava%\test-data\  /q
 if errorlevel 2 pause
 "%zippath%"  "a"  "%distjava%.zip"  "%distjava%\*" > NUL
 echo %distjava%.zip created
@@ -67,15 +67,15 @@ if not exist %distobjc%  md  %distobjc%
 if exist %distobjc%\doc\*  del %distobjc%\doc\*  /q
 if exist %distobjc%\src-o\*  del %distobjc%\src-o\*  /q
 if exist %distobjc%\test-data\*  del %distobjc%\test-data\*  /q
-xcopy ..\doc\objc.html  %distobjc%\doc\  /q
+xcopy ..\AStyleDev\doc\objc.html  %distobjc%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\navy.css  %distobjc%\doc\  /q
+xcopy ..\AStyleDev\doc\navy.css  %distobjc%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\styles.css  %distobjc%\doc\  /q
+xcopy ..\AStyleDev\doc\styles.css  %distobjc%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\src-o\*.m  %distobjc%\src-o\  /q
+xcopy ..\AStyleDev\src-o\*.m  %distobjc%\src-o\  /q
 if errorlevel 2 pause
-xcopy ..\test-data\*  %distobjc%\test-data\  /q
+xcopy ..\AStyleDev\test-data\*  %distobjc%\test-data\  /q
 if errorlevel 2 pause
 "%zippath%"  "a"  "%distobjc%.zip"  "%distobjc%\*" > NUL
 echo %distobjc%.zip created
@@ -88,21 +88,21 @@ if not exist %distpython%  md  %distpython%
 if exist %distpython%\doc\*  del %distpython%\doc\*  /q
 if exist %distpython%\src-p\*  del %distpython%\src-p\*  /q
 if exist %distpython%\test-data\*  del %distpython%\test-data\*  /q
-xcopy ..\doc\python.html  %distpython%\doc\  /q
+xcopy ..\AStyleDev\doc\python.html  %distpython%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\manxome.css  %distpython%\doc\  /q
+xcopy ..\AStyleDev\doc\manxome.css  %distpython%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\styles.css  %distpython%\doc\  /q
+xcopy ..\AStyleDev\doc\styles.css  %distpython%\doc\  /q
 if errorlevel 2 pause
 REM copy only the required programs
-xcopy ..\src-p\ExampleByte.py  %distpython%\src-p\  /q
+xcopy ..\AStyleDev\src-p\ExampleByte.py  %distpython%\src-p\  /q
 if errorlevel 2 pause
-xcopy ..\src-p\ExampleExe.py  %distpython%\src-p\  /q
+xcopy ..\AStyleDev\src-p\ExampleExe.py  %distpython%\src-p\  /q
 if errorlevel 2 pause
-xcopy ..\src-p\ExampleUnicode.py  %distpython%\src-p\  /q
+xcopy ..\AStyleDev\src-p\ExampleUnicode.py  %distpython%\src-p\  /q
 if errorlevel 2 pause
 REM copy the files
-xcopy ..\test-data\*  %distpython%\test-data\  /q
+xcopy ..\AStyleDev\test-data\*  %distpython%\test-data\  /q
 if errorlevel 2 pause
 "%zippath%"  "a"  "%distpython%.zip"  "%distpython%\*" > NUL
 echo %distpython%.zip created
@@ -115,15 +115,15 @@ if not exist %distsharp%  md  %distsharp%
 if exist %distsharp%\doc\*  del %distsharp%\doc\*  /q
 if exist %distsharp%\src-s\*  del %distsharp%\src-s\*  /q
 if exist %distsharp%\test-data\*  del %distsharp%\test-data\*  /q
-xcopy ..\doc\sharp.html  %distsharp%\doc\  /q
+xcopy ..\AStyleDev\doc\sharp.html  %distsharp%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\olive.css  %distsharp%\doc\  /q
+xcopy ..\AStyleDev\doc\olive.css  %distsharp%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\styles.css  %distsharp%\doc\  /q
+xcopy ..\AStyleDev\doc\styles.css  %distsharp%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\src-s\*.cs  %distsharp%\src-s\  /q
+xcopy ..\AStyleDev\src-s\*.cs  %distsharp%\src-s\  /q
 if errorlevel 2 pause
-xcopy ..\test-data\*  %distsharp%\test-data\  /q
+xcopy ..\AStyleDev\test-data\*  %distsharp%\test-data\  /q
 if errorlevel 2 pause
 "%zippath%"  "a"  "%distsharp%.zip"  "%distsharp%\*" > NUL
 echo %distsharp%.zip created
@@ -136,15 +136,15 @@ if not exist %distsharp2%  md  %distsharp2%
 if exist %distsharp2%\doc\*  del %distsharp2%\doc\*  /q
 if exist %distsharp2%\src-s2\*  del %distsharp2%\src-s2\*  /q
 if exist %distsharp2%\test-data\*  del %distsharp2%\test-data\*  /q
-xcopy ..\doc\sharp2.html  %distsharp2%\doc\  /q
+xcopy ..\AStyleDev\doc\sharp2.html  %distsharp2%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\anotherdark.css  %distsharp2%\doc\  /q
+xcopy ..\AStyleDev\doc\anotherdark.css  %distsharp2%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\doc\styles.css  %distsharp2%\doc\  /q
+xcopy ..\AStyleDev\doc\styles.css  %distsharp2%\doc\  /q
 if errorlevel 2 pause
-xcopy ..\src-s2\*.cs  %distsharp2%\src-s2\  /q
+xcopy ..\AStyleDev\src-s2\*.cs  %distsharp2%\src-s2\  /q
 if errorlevel 2 pause
-xcopy ..\test-data\*  %distsharp2%\test-data\  /q
+xcopy ..\AStyleDev\test-data\*  %distsharp2%\test-data\  /q
 if errorlevel 2 pause
 "%zippath%"  "a"  "%distsharp2%.zip"  "%distsharp2%\*" > NUL
 echo %distsharp2%.zip created
